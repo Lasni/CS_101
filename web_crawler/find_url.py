197245e1bf183981f0cfbd005b9bf09040a6b173
Lasni <grega.lasnibat@gmail.com>
2016-06-21 19:17:54 +0000

    find url from html

---
+++ web_crawler/find_url.py
@@ -0,0 +1,14 @@
+"""
+Find the URL in the page HTML and assign it to a variable
+"""
+
+page = ('<div id="top_bin"><div id="top_content" class="width960">'
+        '<div class="udacity float-left"><a href="http://udacity.com">')
+
+start_link = page.find("<a href=")
+start_quote = page.find('"', start_link)
+end_link = page.find(">", start_link)
+end_quote = page.find('"', start_quote+1)
+
+url = page[start_quote+1: end_quote]
+print(url)
