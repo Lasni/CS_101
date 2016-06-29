703da492883742b163142e3efd5f46679f6fcd4d
Lasni <grega.lasnibat@gmail.com>
2016-06-29 16:25:59 +0000

    problem sets

---
+++ problem_sets/word_count.py
@@ -0,0 +1,23 @@
+# Write a procedure, count_words, which takes as input a string
+# and returns the number of words in the string. You may consider words
+# as strings of characters separated by spaces.
+
+
+def count_words(text):
+    spaces = 0
+    if not text:
+        return 0
+    for char in text:
+        if char == ' ':
+            spaces += 1
+    return spaces + 1
+
+
+passage = ("The number of orderings of the 52 cards in a deck of cards "
+           "is so great that if every one of the almost 7 billion people alive "
+           "today dealt one ordering of the cards per second, it would take "
+           "2.5 * 10**40 times the age of the universe to order the cards in every "
+           "possible way.")
+print count_words(passage)
+# >>>56
+
