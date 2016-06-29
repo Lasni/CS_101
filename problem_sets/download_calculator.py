48fda519cae926a42602ff22e8f17ab24b1f9490
Lasni <grega.lasnibat@gmail.com>
2016-06-29 17:07:19 +0000

    problem sets

--- problem_sets/download_calculator.py
+++ problem_sets/download_calculator.py
@@ -9,25 +9,25 @@
 # in kilobits (kb), kilobytes (kB), megabits (Mb), megabytes (MB),
 # gigabits (Gb), gigabytes (GB) and terabits (Tb), terabytes (TB).
 
-#print 2 ** 10      # one kilobit, kb
-#print 2 ** 10 * 8  # one kilobyte, kB
+# print 2 ** 10      # one kilobit, kb
+# print 2 ** 10 * 8  # one kilobyte, kB
 
-#print 2 ** 20      # one megabit, Mb
-#print 2 ** 20 * 8  # one megabyte, MB
+# print 2 ** 20      # one megabit, Mb
+# print 2 ** 20 * 8  # one megabyte, MB
 
-#print 2 ** 30      # one gigabit, Gb
-#print 2 ** 30 * 8  # one gigabyte, GB
+# print 2 ** 30      # one gigabit, Gb
+# print 2 ** 30 * 8  # one gigabyte, GB
 
-#print 2 ** 40      # one terabit, Tb
-#print 2 ** 40 * 8  # one terabyte, TB
+# print 2 ** 40      # one terabit, Tb
+# print 2 ** 40 * 8  # one terabyte, TB
 
 # Often bandwidth is given in megabits (Mb) per second whereas file size
 # is given in megabytes (MB).
 
 
 def download_time(file_size, file_units, bandwidth_size, bandwidth_units):
-    lst = [['kb', 2**10],['kB', 2**10*8],['Mb', 2**20],['MB', 2**20*8],
-           ['Gb', 2**30],['GB', 2**30*8],['Tb', 2**40],['TB', 2**40*8]]
+    lst = [['kb', 2 ** 10], ['kB', 2 ** 10 * 8], ['Mb', 2 ** 20], ['MB', 2 ** 20 * 8],
+           ['Gb', 2 ** 30], ['GB', 2 ** 30 * 8], ['Tb', 2 ** 40], ['TB', 2 ** 40 * 8]]
 
     file_num = 0
     band_num = 0
@@ -37,9 +37,9 @@ def download_time(file_size, file_units, bandwidth_size, bandwidth_units):
         if pair[0] == bandwidth_units:
             band_num = bandwidth_size * pair[1]
 
-    result = float(file_num)/band_num
+    result = float(file_num) / band_num
 
-    h = int(result/3600)
+    h = int(result / 3600)
     m = int(result % 3600 / 60)
     s = result % 3600 % 60
 
@@ -59,22 +59,20 @@ def download_time(file_size, file_units, bandwidth_size, bandwidth_units):
     return hour + minute + second
 
 
-print download_time(1024,'kB', 1, 'MB')
-#>>> 0 hours, 0 minutes, 1 second
+print download_time(1024, 'kB', 1, 'MB')
+# >>> 0 hours, 0 minutes, 1 second
 
-print download_time(1024,'kB', 1, 'Mb')
-#>>> 0 hours, 0 minutes, 8 seconds  # 8.0 seconds is also acceptable
+print download_time(1024, 'kB', 1, 'Mb')
+# >>> 0 hours, 0 minutes, 8 seconds  # 8.0 seconds is also acceptable
 
-print download_time(13,'GB', 5.6, 'MB')
-#>>> 0 hours, 39 minutes, 37.1428571429 seconds
+print download_time(13, 'GB', 5.6, 'MB')
+# >>> 0 hours, 39 minutes, 37.1428571429 seconds
 
-print download_time(13,'GB', 5.6, 'Mb')
-#>>> 5 hours, 16 minutes, 57.1428571429 seconds
+print download_time(13, 'GB', 5.6, 'Mb')
+# >>> 5 hours, 16 minutes, 57.1428571429 seconds
 
-print download_time(10,'MB', 2, 'kB')
-#>>> 1 hour, 25 minutes, 20 seconds  # 20.0 seconds is also acceptable
+print download_time(10, 'MB', 2, 'kB')
+# >>> 1 hour, 25 minutes, 20 seconds  # 20.0 seconds is also acceptable
 
-print download_time(10,'MB', 2, 'kb')
-#>>> 11 hours, 22 minutes, 40 seconds  # 40.0 seconds is also acceptable
-
-
+print download_time(10, 'MB', 2, 'kb')
+# >>> 11 hours, 22 minutes, 40 seconds  # 40.0 seconds is also acceptable
